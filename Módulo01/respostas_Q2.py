ce5cf578d9c0399126cb248d4193aa962905de5e
David Anderson <davidmaceio@hotmail.com>
2023-10-15 13:31:05 -0300

    resposta 2 alterada

--- Módulo01/respostas_Q2.py
+++ Módulo01/respostas_Q2.py
@@ -1,15 +1,9 @@
 print("Questão 02: Faça um programa que peça 4 notas bimestrais e mostre a média")
-n1 = float
-print("Digite a nota do 1 bimestre: ")
-
-n2 = float
-print("Digite a nota do 2 bimestre: ")
-
-n3 = float
-print("Digite a nota do 3 bimestre: ")
-
-n4 = float
-print("Digite a nota do 4 bimestre: ")
-
-media = (n1 + n2 + n3 + n4)/4
-print("Média", media)
+n1 = int (input ("Digite a nota do 1 bimestre: "))
+n2 = int (input ("Digite a nota do 2 bimestre: "))
+n3 = int (input ("Digite a nota do 3 bimestre: "))
+n4 = int (input ("Digite a nota do 4 bimestre: "))
+soma = int
+media = int (soma/4)
+soma = n1 + n2 + n3 + n4
+print(" A sua média foi: ", media)
